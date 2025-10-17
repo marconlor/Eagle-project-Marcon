a1c42bacae935c4897115c375df23a1ec37fbef8
marconlor <marcon.lorenzo01@gmail.com>
2025-10-17 13:42:42 +0200

    project setup

---
+++ build/CMakeFiles/Project2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Project2.dir/fake_receiver.c.o"
+  "CMakeFiles/Project2.dir/fake_receiver.c.o.d"
+  "libProject2.pdb"
+  "libProject2.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Project2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/project_2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/home/lorenzo/CLionProjects/Eagle-project-Marcon/bin/project_2"
+  "/home/lorenzo/CLionProjects/Eagle-project-Marcon/bin/project_2.pdb"
+  "CMakeFiles/project_2.dir/main.cpp.o"
+  "CMakeFiles/project_2.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
